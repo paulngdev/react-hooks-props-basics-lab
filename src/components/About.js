fb9517a01c49737aaf5e9ff44d74e1b489c81509
PaulNguyen <paulng2207.dev@gmail.com>
2024-02-26 13:22:46 +1100

    Error Detect

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About ({bio, sociallist}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
-}
+      {bio ? <p> {bio} </p> : null}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />;  
+      <Links github={sociallist.github} linkedin={sociallist.linkedin}/>
+    </div>  
+
+  )}
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,16 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+// pass this data down as props to the child component(s) that need it!
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar  />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio= {user.bio} sociallist={user.links}/>
+      
     </div>
   );
 }
